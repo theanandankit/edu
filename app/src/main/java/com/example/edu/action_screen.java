31b33d963b4e57ad46630df5adc77df14e0e1e98
theanandankit <ankitanand2909@gmail.com>
2019-08-11 16:32:20 +0530

    action screen updated

--- app/src/main/java/com/example/edu/action_screen.java
+++ app/src/main/java/com/example/edu/action_screen.java
@@ -1,14 +1,41 @@
 package com.example.edu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class action_screen extends AppCompatActivity {
+public class action_screen extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_action_screen);
+
+        CardView mana= findViewById(R.id.management);
+
+        mana.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        Intent i;
+
+        switch (view.getId())
+        {
+            case R.id.management:
+                i=new Intent(this,login_screen.class);
+                startActivity(i);
+                break;
+
+            case R.id.teaching:
+                 i=new Intent(this,login_screen.class);
+                 startActivity(i);
+                 break;
+
+        }
     }
 }
