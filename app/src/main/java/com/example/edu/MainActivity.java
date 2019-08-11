a30c219fcd0d719051c10c71bde9ad17b55c8e19
theanandankit <ankitanand2909@gmail.com>
2019-08-11 10:43:41 +0530

    splash screen added

--- app/src/main/java/com/example/edu/MainActivity.java
+++ app/src/main/java/com/example/edu/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.edu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int splash_time_out=4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeintent= new Intent(MainActivity.this,splash_screen.class);
+                startActivity(homeintent);
+                finish();
+            }
+        },splash_time_out);
     }
 }
