dc8138f88a06b0b0ccf180ed5bb6401404f08d96
theanandankit <ankitanand2909@gmail.com>
2019-08-13 16:27:41 +0530

    update

--- app/src/main/java/com/example/edu/action_screen.java
+++ app/src/main/java/com/example/edu/action_screen.java
@@ -15,8 +15,13 @@ public class action_screen extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_action_screen);
 
         CardView mana= findViewById(R.id.management);
+        CardView teach=findViewById(R.id.teaching);
+        CardView admin=findViewById(R.id.admin);
+
 
         mana.setOnClickListener(this);
+        teach.setOnClickListener(this);
+        admin.setOnClickListener(this);
     }
 
     @Override
@@ -35,6 +40,10 @@ public class action_screen extends AppCompatActivity implements View.OnClickList
                  i=new Intent(this,login_screen.class);
                  startActivity(i);
                  break;
+            case R.id.admin:
+                i=new Intent(this,Management_screen.class);
+                startActivity(i);
+                break;
 
         }
     }
