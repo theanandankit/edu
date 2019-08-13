e7285b9852fe563718d11ae11171bcc29e58ce42
theanandankit <ankitanand2909@gmail.com>
2019-08-14 02:49:40 +0530

    login_layout updated

--- app/src/main/java/com/example/edu/action_screen.java
+++ app/src/main/java/com/example/edu/action_screen.java
@@ -5,7 +5,12 @@ import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import com.google.firebase.FirebaseApiNotAvailableException;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class action_screen extends AppCompatActivity implements View.OnClickListener {
 
@@ -13,7 +18,9 @@ public class action_screen extends AppCompatActivity implements View.OnClickList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_action_screen);
-
+        Log.e("action_screen.java","working");
+        DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReference().child("hello");
+        databaseReference.setValue("bye");
         CardView mana= findViewById(R.id.management);
         CardView teach=findViewById(R.id.teaching);
         CardView admin=findViewById(R.id.admin);
--- app/src/main/java/com/example/edu/login_screen.java
+++ app/src/main/java/com/example/edu/login_screen.java
@@ -4,9 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class login_screen extends AppCompatActivity {
@@ -19,7 +24,37 @@ public class login_screen extends AppCompatActivity {
         final EditText use=findViewById(R.id.username);
         final EditText pass=findViewById(R.id.password);
 
+        String text1="Forget Password";
+        String text2="Trouble In Login";
+
         Button login=findViewById(R.id.login);
+        TextView forget_password=findViewById(R.id.forget_password);
+        TextView trouble=findViewById(R.id.trouble);
+
+        SpannableString spannableString=new SpannableString(text1);
+        SpannableString spannableString1=new SpannableString(text2);
+
+        ClickableSpan clickableSpan =new ClickableSpan() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(login_screen.this,"Forget password",Toast.LENGTH_LONG).show();
+            }
+        };
+
+        ClickableSpan clickableSpan1 = new ClickableSpan() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(login_screen.this,"trouble in login",Toast.LENGTH_LONG).show();
+            }
+        };
+
+        spannableString.setSpan(clickableSpan,0,15, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString1.setSpan(clickableSpan1,0,16,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        forget_password.setText(spannableString);
+        forget_password.setMovementMethod(LinkMovementMethod.getInstance());
+        trouble.setText(spannableString1);
+        trouble.setMovementMethod(LinkMovementMethod.getInstance());
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
