21a49dbde79f9fa039a25e8538d0d00799e66cdc
theanandankit <ankitanand2909@gmail.com>
2019-08-16 05:52:18 +0530

    update

--- app/src/main/java/com/example/edu/Management_screen.java
+++ app/src/main/java/com/example/edu/Management_screen.java
@@ -3,6 +3,9 @@ package com.example.edu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class Management_screen extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class Management_screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_management_screen);
+
+        ArrayList<TextView> name_teacher= new ArrayList<>();
+
+        name_teacher.add(0, (TextView) findViewById(R.id.tech1));
+
+
+        name_teacher.get(0).setText("Ankit kumar");
     }
 }
