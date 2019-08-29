cf5f12a6702f781b915831cb7908182539ff798c
theanandankit <ankitanand2909@gmail.com>
2019-08-29 23:58:16 +0530

    firebase new user

--- app/src/main/java/com/example/edu/login_screen.java
+++ app/src/main/java/com/example/edu/login_screen.java
@@ -20,10 +20,13 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class login_screen extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +38,7 @@ public class login_screen extends AppCompatActivity {
 
 
         firebaseAuth =FirebaseAuth.getInstance();
+
 
         String text1="Forget Password";
         String text2="Trouble In Login";
--- app/src/main/java/com/example/edu/new_registration.java
+++ app/src/main/java/com/example/edu/new_registration.java
@@ -13,11 +13,19 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class new_registration extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    DatabaseReference databaseReference,databaseReference_counter;
 
+    public static String counter;
+    public static int user_counter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +33,8 @@ public class new_registration extends AppCompatActivity {
 
         final EditText user = findViewById(R.id.user);
         final EditText pass = findViewById(R.id.pass);
-
+        final EditText uname=findViewById(R.id.user_name);
+        final EditText ubatch=findViewById(R.id.user_batch);
 
         Button regi = findViewById(R.id.register);
 
@@ -35,6 +44,8 @@ public class new_registration extends AppCompatActivity {
 
                 final String username = user.getText().toString();
                 final String password = pass.getText().toString();
+                String uuname=uname.getText().toString();
+                String uubatch=ubatch.getText().toString();
 
                 if (username.isEmpty()) {
                     Toast.makeText(new_registration.this, "please Enter the Username", Toast.LENGTH_LONG).show();
@@ -45,21 +56,70 @@ public class new_registration extends AppCompatActivity {
                     return;
                 }
 
+                if(uuname.isEmpty())
+                {
+                    Toast.makeText(new_registration.this, "please Enter the Name", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                if(uubatch.isEmpty())
+                {
+                    Toast.makeText(new_registration.this, "please Enter the batch", Toast.LENGTH_LONG).show();
+                    return;
+                }
                 firebaseAuth.createUserWithEmailAndPassword(username, password)
                         .addOnCompleteListener(new_registration.this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
 
                                 if (task.isSuccessful()) {
-                                    Toast.makeText(new_registration.this, "ok", Toast.LENGTH_LONG).show();
+                                    Toast.makeText(new_registration.this, username+" is registered successfully", Toast.LENGTH_LONG).show();
 
                                 } else {
-                                    Toast.makeText(new_registration.this, "not", Toast.LENGTH_LONG).show();
+                                    Toast.makeText(new_registration.this, "Something went gone \n Please try again later", Toast.LENGTH_LONG).show();
                                 }
                             }
                         });
+
+                new_user userr=new new_user(uuname,username,uubatch);
+
+                databaseReference=FirebaseDatabase.getInstance().getReference().child("teacher_info").child(counter);
+
+                databaseReference.child("user").setValue(userr);
+
+                user_counter++;
+                databaseReference_counter.setValue(user_counter);
             }
         });
 
+        databaseReference_counter=FirebaseDatabase.getInstance().getReference().child("teacher_info").child("counter");
+
+        databaseReference_counter.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                counter=dataSnapshot.getValue().toString();
+                user_counter=dataSnapshot.getValue().hashCode();
+                Toast.makeText(new_registration.this,counter,Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
+    public class new_user{
+        public String user_name;
+        public String email;
+        public String batch;
+
+        public new_user(String user_name,String email,String batch)
+        {
+            this.user_name=user_name;
+            this.batch=batch;
+            this.email=email;
+        }
+    }
+
 }
