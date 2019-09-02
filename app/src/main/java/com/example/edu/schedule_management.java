17a60cab730f0ac004878a9361221a029106db12
theanandankit <ankitanand2909@gmail.com>
2019-09-02 20:41:28 +0530

    teacher edit

--- app/src/main/java/com/example/edu/schedule_management.java
+++ app/src/main/java/com/example/edu/schedule_management.java
@@ -2,23 +2,35 @@ package com.example.edu;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.app.Dialog;
+import android.app.usage.UsageEvents;
 import android.os.Bundle;
+import android.util.EventLog;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.core.view.Event;
 
 import java.util.ArrayList;
 
 public class schedule_management extends AppCompatActivity {
 
 
+    CardView cardView;
     DatabaseReference databaseReference;
     ArrayList<String> teacher_name=new ArrayList<>();
     @Override
@@ -29,6 +41,16 @@ public class schedule_management extends AppCompatActivity {
         Spinner spinner=findViewById(R.id.spinner2);
         Button button=findViewById(R.id.check_button);
 
+        cardView=findViewById(R.id.card1);
+
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                showCustomDialog(R.id.teacher1,R.id.sub1);
+
+            }
+        });
 
 
         for (int a=1;a<13;a++) {
@@ -51,5 +73,43 @@ public class schedule_management extends AppCompatActivity {
             aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
             spinner.setAdapter(aa);
         }
+    }
+    private void showCustomDialog(final int a,final int b) {
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE); // before
+        dialog.setContentView(R.layout.dialog_event);
+        dialog.setCancelable(true);
+
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+        lp.copyFrom(dialog.getWindow().getAttributes());
+        lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+
+
+        ((ImageButton) dialog.findViewById(R.id.bt_close)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        ((Button) dialog.findViewById(R.id.bt_save)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                TextView aa=findViewById(a);
+                EditText teacher_name=findViewById(R.id.set_teacher_name);
+                String teacher=teacher_name.getText().toString();
+             //   aa.setText(teacher);
+                TextView bb=findViewById(b);
+                EditText subject_name=findViewById(R.id.set_subject);
+                String subject=subject_name.getText().toString();
+              //  bb.setText(subject);
+
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
+        dialog.getWindow().setAttributes(lp);
     }
 }
