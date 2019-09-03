77a4caf53b2fc2d9e6df11f2abcf12dfae914037
theanandankit <ankitanand2909@gmail.com>
2019-09-03 06:28:15 +0530

    admin page

--- app/src/main/java/com/example/edu/schedule_management.java
+++ app/src/main/java/com/example/edu/schedule_management.java
@@ -5,16 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.app.Dialog;
-import android.app.usage.UsageEvents;
+
 import android.os.Bundle;
-import android.util.EventLog;
+
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
+
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -23,27 +23,27 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.database.core.view.Event;
 
 import java.util.ArrayList;
 
 public class schedule_management extends AppCompatActivity {
 
-
-    CardView cardView;
+    CardView cardView1,cardView3,cardView5,cardView6,cardView7,cardView8,cardView9,cardView10,cardView11,cardView12;
     DatabaseReference databaseReference;
     ArrayList<String> teacher_name=new ArrayList<>();
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule_management);
 
         Spinner spinner=findViewById(R.id.spinner2);
         Button button=findViewById(R.id.check_button);
 
-        cardView=findViewById(R.id.card1);
 
-        cardView.setOnClickListener(new View.OnClickListener() {
+        findids();
+
+        cardView1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
@@ -51,6 +51,63 @@ public class schedule_management extends AppCompatActivity {
 
             }
         });
+
+        cardView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showCustomDialog(R.id.teacher3,R.id.sub3);
+            }
+        });
+        cardView5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showCustomDialog(R.id.teacher5,R.id.sub5);
+            }
+        });
+
+        cardView6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showCustomDialog(R.id.teacher6,R.id.sub6);
+            }
+        });
+        cardView7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showCustomDialog(R.id.teacher7,R.id.sub7);
+            }
+        });
+        cardView8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showCustomDialog(R.id.teacher8,R.id.sub8);
+            }
+        });
+        cardView9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showCustomDialog(R.id.teacher9,R.id.sub9);
+            }
+        });
+        cardView10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showCustomDialog(R.id.teacher10,R.id.sub10);
+            }
+        });
+        cardView11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showCustomDialog(R.id.teacher11,R.id.sub11);
+            }
+        });
+        cardView12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showCustomDialog(R.id.teacher12,R.id.sub12);
+            }
+        });
+
 
 
         for (int a=1;a<13;a++) {
@@ -86,24 +143,25 @@ public class schedule_management extends AppCompatActivity {
         lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
 
 
-        ((ImageButton) dialog.findViewById(R.id.bt_close)).setOnClickListener(new View.OnClickListener() {
+        (dialog.findViewById(R.id.bt_close)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dialog.dismiss();
             }
         });
-        ((Button) dialog.findViewById(R.id.bt_save)).setOnClickListener(new View.OnClickListener() {
+        (dialog.findViewById(R.id.bt_save)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                TextView aa=findViewById(a);
-                EditText teacher_name=findViewById(R.id.set_teacher_name);
-                String teacher=teacher_name.getText().toString();
-             //   aa.setText(teacher);
-                TextView bb=findViewById(b);
-                EditText subject_name=findViewById(R.id.set_subject);
-                String subject=subject_name.getText().toString();
-              //  bb.setText(subject);
+                String teacher,subject;
+                EditText teacher_nam = dialog.findViewById(R.id.set_teacher_name);
+                teacher = teacher_nam.getText().toString();
+                TextView aa = findViewById(a);
+                aa.setText(teacher);
+                EditText subject_nam= dialog.findViewById(R.id.set_subject);
+                subject = subject_nam.getText().toString();
+                TextView bb = findViewById(b);
+                bb.setText(subject);
 
                 dialog.dismiss();
             }
@@ -111,5 +169,21 @@ public class schedule_management extends AppCompatActivity {
 
         dialog.show();
         dialog.getWindow().setAttributes(lp);
+    }
+
+    public void findids()
+    {
+        cardView1=findViewById(R.id.card1);
+        cardView3=findViewById(R.id.card3);
+        cardView5=findViewById(R.id.card5);
+        cardView6=findViewById(R.id.card6);
+        cardView7=findViewById(R.id.card7);
+        cardView8=findViewById(R.id.card8);
+        cardView9=findViewById(R.id.card9);
+        cardView10=findViewById(R.id.card10);
+        cardView11=findViewById(R.id.card11);
+        cardView12=findViewById(R.id.card12);
+
+
     }
 }
