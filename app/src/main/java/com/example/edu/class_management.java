efeb34890ca2fb3f35ee7f5ca4040337b5fc4029
theanandankit <ankitanand2909@gmail.com>
2019-09-20 12:07:17 +0530

    class management updated

--- app/src/main/java/com/example/edu/class_management.java
+++ app/src/main/java/com/example/edu/class_management.java
@@ -9,6 +9,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class class_management extends AppCompatActivity {
 
@@ -20,7 +24,6 @@ public class class_management extends AppCompatActivity {
         setContentView(R.layout.activity_class_management);
 
         subtitute=findViewById(R.id.subtitute);
-        merge=findViewById(R.id.merge);
         problem=findViewById(R.id.problem);
         nostudent=findViewById(R.id.nostudent);
         subtitute.setOnClickListener(new View.OnClickListener() {
@@ -30,12 +33,11 @@ public class class_management extends AppCompatActivity {
                 showdilog_subtitue();
             }
         });
-        merge.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.merge).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                Intent ii=new Intent(class_management.this,merge_popup.class);
-                startActivity(ii);
+                showdilog_merge();
             }
         });
         nostudent.setOnClickListener(new View.OnClickListener() {
@@ -58,6 +60,26 @@ public class class_management extends AppCompatActivity {
 
     private void showdilog_merge()
     {
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE); // before
+        dialog.setContentView(R.layout.merge_popup);
+        dialog.setCancelable(true);
+
+        WindowManager.LayoutParams lp = new WindowManager.LayoutParams();
+        lp.copyFrom(dialog.getWindow().getAttributes());
+        lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+        lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
+
+        String class_name[]={"1","2","3","4","5","6","7","8","9","10","11","12"};
+
+        Spinner spinner=dialog.findViewById(R.id.spinner1);
+
+        ArrayAdapter aa=new ArrayAdapter(class_management.this,android.R.layout.simple_spinner_item,class_name);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(aa);
+
+        dialog.show();
+        dialog.getWindow().setAttributes(lp);
 
     }
     private void showdilog_subtitue()
@@ -74,7 +96,51 @@ public class class_management extends AppCompatActivity {
         lp.height = WindowManager.LayoutParams.WRAP_CONTENT;
 
 
-        (dialog.findViewById(R.id.bt_close)).setOnClickListener(new View.OnClickListener() {
+
+        final EditText name_teacher=dialog.findViewById(R.id.name);
+        final EditText batch_teacher=dialog.findViewById(R.id.Batch);
+
+        final CheckBox check_member=dialog.findViewById(R.id.check_member);
+        final CheckBox check_not_member=dialog.findViewById(R.id.check_not_member);
+
+        final String name[]={"Amit","Aayush","Ankit","Divyansh","kamal","Shivam","Vishnu"};
+
+        final Spinner spinner=dialog.findViewById(R.id.spinner);
+
+        ArrayAdapter<String> aa=new ArrayAdapter<>(class_management.this,android.R.layout.simple_spinner_item,name);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(aa);
+
+        check_member.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(check_member.isChecked())
+                {
+                    spinner.setEnabled(true);
+                    name_teacher.setEnabled(false);
+                    batch_teacher.setEnabled(false);
+                    check_not_member.setChecked(false);
+                }
+            }
+        });
+        check_not_member.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                if(check_not_member.isChecked())
+                {
+                    spinner.setEnabled(false);
+                    name_teacher.setEnabled(true);
+                    batch_teacher.setEnabled(true);
+                    check_member.setChecked(false);
+                }
+            }
+        });
+
+
+    (dialog.findViewById(R.id.bt_close)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dialog.dismiss();
